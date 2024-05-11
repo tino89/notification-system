fd8345e9ba9f5a2b6389e012d3021a6d45816fb8
Tino chagua <tinochagua@gmail.com>
2024-05-11 12:09:50 -0500

    solving test

--- back/test/notification.test.js
+++ back/test/notification.test.js
@@ -1,8 +1,8 @@
 const request = require("supertest")
 const app = require("../app")
-const notification = require("../database/repository/notificationRepository")
+//const notification = require("../database/repository/notificationRepository")
 
-jest.mock('notification');
+//jest.mock('notification');
 
 describe("Testing notification endpoint", () => {
     
